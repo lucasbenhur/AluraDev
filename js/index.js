f5abf00a185732f2f5a26cb9f25946a6d2f52ab0
lucas.alcantara <lucas.alcantara@hubconexa.com>
2021-06-12 19:16:51 -0300

    Implementado a função de salvar projeto e listar os projetos salvos na página da comunidade.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var menuCodeEditor = $("#menu-code-editor");
+let menuCodeEditor = $("#menu-code-editor");
 menuCodeEditor.addClass("nav-link-active");
 $(menuCodeEditor[0].firstElementChild).addClass("btn-item-menu-active");
 
@@ -17,8 +17,8 @@ $("#menu-code-editor").on("click", function () {
     $(this).addClass("nav-link-active");
     $(this.firstElementChild).addClass("btn-item-menu-active");
 
-    $("#col-code-editor").css("display", "initial");
-    $("#col-formulario").css("display", "initial");
+    $("#col-code-editor").css("display", "block");
+    $("#col-formulario").css("display", "block");
     $("#col-comunidade").css("display", "none");
 });
 
@@ -28,7 +28,9 @@ $("#menu-comunidade").on("click", function () {
 
     $("#col-code-editor").css("display", "none");
     $("#col-formulario").css("display", "none");
-    $("#col-comunidade").css("display", "initial");
+    $("#col-comunidade").css("display", "block");
+
+    carregaProjetos();
 });
 
 $("#btn-highligth").on("click", function() {
@@ -90,7 +92,7 @@ $(".nav-link").on("click", function () {
 
 window.addEventListener('resize', function () {
     /*var heigth = window.innerHeight; */
-    var width = window.innerWidth;
+    let width = window.innerWidth;
 
     if (width >= 992) {
         defaultMenu();
@@ -113,7 +115,7 @@ function defaultColunasNavbar () {
 };
 
 function salvar () {
-    var projeto = {
+    let projeto = {
         "nome": $("#txt-nome-projeto").val(),
         "descricao": $("#txt-descricao-projeto").val(),
         "linguagem": $("#select-linguagem").val(),
@@ -124,16 +126,82 @@ function salvar () {
     localStorage.setItem(newGuid(), JSON.stringify(projeto));
 };
 
-function newGuid()  
-{  
-   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {  
-      var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);  
-      return v.toString(16);  
-   });  
+function newGuid() {  
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {  
+        let r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);  
+        return v.toString(16);  
+    });
 };
 
-$(".card .btn-favorito").on("click", function () {
-    var img_favorito = $(this.firstElementChild);
+function carregaProjetos() {
+
+    let htmlProjetos = "";
+    
+    $("#col-comunidade").html(htmlProjetos);
+
+    let index = 0;
+
+    Object.keys(localStorage).forEach((key) => {
+
+        index++;
+
+        let projeto = JSON.parse(localStorage.getItem(key));
+
+        if (index % 2 !== 0) {
+            htmlProjetos += '<div class="row">' +
+                                '<div class="col-left col-lg-6">';
+        } else {
+            htmlProjetos += '<div class="col-right col-lg-6">';
+        }
+        
+        htmlProjetos += '<div class="cartao"> ' +
+                            '<div class="bg-code-editor" style="background: ' + projeto.cor +';">' +
+                                '<div class="btn-mac">' +
+                                    '<img class="img-mac" src="img/mac_buttons.svg">' +
+                                '</div>' +
+                                '<div class="div-code-editor">' +
+                                    '<code class="code-editor" contenteditable="false" spellcheck="false">' +
+                                        hljs.highlight(projeto.codigo, {language: projeto.linguagem}).value +
+                                    '</code>' +
+                                '</div>' +
+                            '</div>' +
+                            '<div class="titulo font-title">' +
+                                projeto.nome +
+                            '</div>' +
+                            '<div class="descricao font-body">' +
+                                projeto.descricao +
+                            '</div>' +
+                            '<div class="row actions">' +
+                                '<div class="col col-lg-5" style="display: flex;">' +
+                                    '<button class="btn-comentario">' +
+                                        '<img class="img-comentario" src="img/icon_comentario.svg">' +
+                                        '9' +
+                                    '</button>' +
+                                    '<button class="btn-favorito">' +
+                                        '<img class="img-favorito" src="img/icon_favorito.svg">' +
+                                        '9' +
+                                    '</button>' +
+                                '</div>' +
+                                '<div class="col col-lg-7" style="text-align: -webkit-right;">' +
+                                    '<button class="btn-usuario">' +
+                                        '<img src="https://avatars.githubusercontent.com/u/46672988?v=4" class="rounded-circle img-usuario" width="32px" height="32px">' +
+                                        '@lucas' +
+                                    '</button>' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>' +
+                    '</div>'
+
+        if (index % 2 === 0) {
+            htmlProjetos += '</div>';
+        }
+    });
+
+    $("#col-comunidade").html(htmlProjetos);
+};
+
+$(".cartao .btn-favorito").on("click", function () {
+    let img_favorito = $(this.firstElementChild);
     
     if (img_favorito.attr("src") === "img/icon_favorito.svg") {
         img_favorito.attr("src", "img/icon_favorito_red.svg");
