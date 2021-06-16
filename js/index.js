ec7e4fca0e85a896da79c13b33fba3d5445832ab
lucas.alcantara <lucas.alcantara@hubconexa.com>
2021-06-16 00:06:32 -0300

    Melhorias + sweetalert para modals.

--- js/index.js
+++ js/index.js
@@ -114,17 +114,57 @@ function defaultColunasNavbar () {
     $("#col-btn-nav").removeAttr("style");
 };
 
-function salvar () {
-    let projeto = {
-        "nome": $("#txt-nome-projeto").val(),
-        "descricao": $("#txt-descricao-projeto").val(),
-        "linguagem": $("#select-linguagem").val(),
-        "cor": $("#color").val(),
-        "codigo": $("#code-editor")[0].innerText
+$("#btn-salvar").on("click", function () {
+    let isValid = true;
+    let listaDeErros = "<ul>";
+    let nomeDoProjeto = $("#txt-nome-projeto")[0];
+    let descricaoDoProjeto = $("#txt-descricao-projeto")[0];
+    let codeEditor = $("#code-editor")[0];
+
+    if (nomeDoProjeto.value === "") {
+        isValid = false;
+        listaDeErros += "<li>Insira o <b>nome</b> do seu projeto!</li>";
     }
 
-    localStorage.setItem(newGuid(), JSON.stringify(projeto));
-};
+    if (descricaoDoProjeto.value === "") {
+        isValid = false;
+        listaDeErros += "<li>Insira a <b>descrição</b> do seu projeto!</li>";
+    }
+
+    if (codeEditor.innerText === "") {
+        isValid = false;
+        listaDeErros += "<li>Insira o <b>código</b> do seu projeto!</li>";
+    }
+
+    listaDeErros += "</ul>"
+
+    if (isValid === true){
+        let projeto = {
+            "nome": $("#txt-nome-projeto").val(),
+            "descricao": $("#txt-descricao-projeto").val(),
+            "linguagem": $("#select-linguagem").val(),
+            "cor": $("#color").val(),
+            "codigo": codeEditor.innerText,
+            "favorito": "N"
+        }
+
+        localStorage.setItem(newGuid(), JSON.stringify(projeto));
+
+        Swal.fire({
+            icon: 'success',
+            title: 'Sucesso!',
+            text: 'O projeto será exibido na página da comunidade.',
+            confirmButtonColor: '#5081FB'
+        }).then(() => { limpaCampos() });
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Erro!',
+            html: listaDeErros,
+            confirmButtonColor: '#5081FB'
+        });
+    }       
+});
 
 function newGuid() {  
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {  
@@ -136,6 +176,7 @@ function newGuid() {
 function carregaProjetos() {
 
     let htmlProjetos = "";
+    let htmlImgFavorito = "";
     
     $("#col-comunidade").html(htmlProjetos);
 
@@ -153,8 +194,14 @@ function carregaProjetos() {
         } else {
             htmlProjetos += '<div class="col-right col-lg-6">';
         }
+
+        if (projeto.favorito === "S") {
+            htmlImgFavorito = '<img class="img-favorito" src="img/icon_favorito_red.svg" data-favorito="S">';
+        } else {
+            htmlImgFavorito = '<img class="img-favorito" src="img/icon_favorito.svg" data-favorito="N">';
+        }
         
-        htmlProjetos += '<div class="cartao"> ' +
+        htmlProjetos += '<div id="' + key + '" class="cartao"> ' +
                             '<div class="bg-code-editor" style="background: ' + projeto.cor +';">' +
                                 '<div class="btn-mac">' +
                                     '<img class="img-mac" src="img/mac_buttons.svg">' +
@@ -177,9 +224,15 @@ function carregaProjetos() {
                                         '<img class="img-comentario" src="img/icon_comentario.svg">' +
                                         '9' +
                                     '</button>' +
-                                    '<button class="btn-favorito">' +
-                                        '<img class="img-favorito" src="img/icon_favorito.svg">' +
+                                    '<button class="btn-favorito" onclick="favoritaProjeto(\'' + key + '\')">' +
+                                        htmlImgFavorito +
                                         '9' +
+                                    '</button>' +
+                                    '<button class="btn-excluir" onclick="excluiProjeto(\'' + key + '\')">' +
+                                        '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">' +
+                                            '<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>' +
+                                            '<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>' +
+                                        '</svg>' +
                                     '</button>' +
                                 '</div>' +
                                 '<div class="col col-lg-7" style="text-align: -webkit-right;">' +
@@ -200,12 +253,50 @@ function carregaProjetos() {
     $("#col-comunidade").html(htmlProjetos);
 };
 
-$(".cartao .btn-favorito").on("click", function () {
-    let img_favorito = $(this.firstElementChild);
+function favoritaProjeto(idProjeto) {
+    let img_favorito = $("#"+idProjeto + " .img-favorito");
     
-    if (img_favorito.attr("src") === "img/icon_favorito.svg") {
+    if (img_favorito.attr("data-favorito") === "N") {
+        img_favorito.attr("data-favorito", "S");
         img_favorito.attr("src", "img/icon_favorito_red.svg");
     } else {
+        img_favorito.attr("data-favorito", "N");
         img_favorito.attr("src", "img/icon_favorito.svg");
     }
-});
+
+    let projeto = JSON.parse(localStorage.getItem(idProjeto));
+    projeto.favorito = img_favorito.attr("data-favorito");
+    localStorage.setItem(idProjeto, JSON.stringify(projeto));
+};
+
+function excluiProjeto(idProjeto) {
+    Swal.fire({
+        title: 'Tem certeza?',
+        text: "Você não poderá reverter isso!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#5081FB',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Sim, excluir!',
+        cancelButtonText: 'Cancelar'
+    }).then((result) => {
+        localStorage.removeItem(idProjeto);
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: 'Excluído!',
+                text: 'O projeto foi excluído.',
+                icon: 'success',
+                confirmButtonColor: '#5081FB'
+            }).then(() => {carregaProjetos();});
+        }
+    });
+};
+
+function limpaCampos() {
+    $("#code-editor")[0].innerText = "";
+    $("#txt-nome-projeto")[0].value = "";
+    $("#txt-descricao-projeto")[0].value = "";
+    $("#select-linguagem")[0].value = "javascript";
+    $("#color")[0].value = "#6BD1FF";
+    $("#col-code-editor .bg-code-editor").css("background", "#6BD1FF");
+};
